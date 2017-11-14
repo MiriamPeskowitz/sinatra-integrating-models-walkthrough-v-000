5e1157dbfdf11c97a8e49d01006717b2cf7459fd
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-14 21:29:47 +0000

    automatically backed up by learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,1 +1,28 @@
 # Your TextAnalyzer model code will go here.
+class TextAnalyzer
+  attr_reader :text
+
+<h1>Your Text Analysis</h1>
+
+<h2>Number of Words: </h2>
+
+<h2>Vowels: </h2>
+
+<h2>Consonants: </h2>
+
+<h2>Most Common Letter: _____ used _____ times</h2>
+
+
+def initialize(text)
+  @text = text.downcase 
+end 
+
+def count_of_words(text)
+  words = text.split(" ")
+  words.count
+end 
+
+def 
+end 
+
+end 
