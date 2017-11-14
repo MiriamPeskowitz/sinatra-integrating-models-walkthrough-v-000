3b1e7344c95a3d88ad1d90f0bb34e66c6dc27483
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-14 21:32:22 +0000

    automatically backed up by learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -13,16 +13,40 @@ class TextAnalyzer
 <h2>Most Common Letter: _____ used _____ times</h2>
 
 
-def initialize(text)
-  @text = text.downcase 
-end 
+  def initialize(text)
+    @text = text.downcase
+  end
 
-def count_of_words(text)
-  words = text.split(" ")
-  words.count
-end 
+  def count_of_words(text)
+    words = text.split(" ")
+    words.count
+  end
 
-def 
-end 
+  def count_of_vowels(text)
+    text.scan(/[aeiou]/).count
+  end
 
+  def count_of_consonants(text)
+    text.scan(/[bcdfghjklmnpqrstvwxyz]/]).count
 end 
+  def most_used_letters 
+    s1 = text.gsub(/[^a-z]/, '') # gets rid of spaces
+    arr = s1.split('')
+    arr1 = arr.uniq
+    arr2 = {}
+ 
+    arr1.map do |c|
+      arr2[c] =  arr.count(c)
+    end
+ 
+    biggest = { arr2.keys.first => arr2.values.first }
+ 
+    arr2.each do |key, value|
+      if value > biggest.values.first
+        biggest = {}
+        biggest[key] = value
+      end
+    end
+    biggest
+  end
+end
