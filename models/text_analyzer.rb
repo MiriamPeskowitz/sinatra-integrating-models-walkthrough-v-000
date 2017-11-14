fbbc67c4ea31e38bd0defd47af05f19c9a002a9f
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-14 21:33:43 +0000

    automatically backed up by learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -28,19 +28,19 @@ class TextAnalyzer
 
   def count_of_consonants(text)
     text.scan(/[bcdfghjklmnpqrstvwxyz]/]).count
-end 
-  def most_used_letters 
+end
+  def most_used_letters
     s1 = text.gsub(/[^a-z]/, '') # gets rid of spaces
     arr = s1.split('')
     arr1 = arr.uniq
     arr2 = {}
- 
+
     arr1.map do |c|
       arr2[c] =  arr.count(c)
     end
- 
+
     biggest = { arr2.keys.first => arr2.values.first }
- 
+
     arr2.each do |key, value|
       if value > biggest.values.first
         biggest = {}
